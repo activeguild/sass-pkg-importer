f52012c653801e525a91c7a6e1aa4f0a245665d9
j1ngzoue <workdrive0306@gmail.com>
2024-09-24 21:13:09 +0900

    fix: Initial

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { NodePackageImporter } from "sass-embedded";
+import { NodePackageImporter } from "sass";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +9,7 @@ export default defineConfig({
       scss: {
         api: "modern",
         importers: [new NodePackageImporter()],
+        additionalData: `@use "pkg:bulma/sass/utilities" as utilities;`,
       },
     },
   },
